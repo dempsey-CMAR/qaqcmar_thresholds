82041ba2e9fd1739a39e49d45278229fb386fcc7
Danielle Dempsey <ddempsey@perennia.ca>
2023-01-09 15:57:32 -0400

    fix vemco vs vr2ar discrepancy
    start adding reactive vars

--- app.R
+++ app.R
@@ -10,6 +10,8 @@ library(qaqcmar)
 library(tidyr)
 
 
+# perhaps move this to script in /R folder --------------------------------
+
 deployment <- "Borgles Island 2019-05-30"
 
 dat <- fread(paste0(here("data"), "/", deployment, ".csv"))
@@ -23,7 +25,7 @@ qc_tests <- c("climatology", "grossrange", "spike")
 
 #sensors <- sort(unique(dat$sensor_type))
 
-thresholds <- threshold_tables$climatology_table %>%
+thresh_all <- threshold_tables$climatology_table %>%
   pivot_longer(cols = c("season_min", "season_max"), names_to = "threshold") %>%
   mutate(threshold = str_remove(threshold, pattern = "season_"),
          threshold = paste(season, threshold, sep = "_")) %>%
@@ -32,12 +34,17 @@ thresholds <- threshold_tables$climatology_table %>%
   bind_rows(
     threshold_tables$grossrange_table %>%
       pivot_longer(cols = sensor_min:user_max, names_to = "threshold") %>%
-      mutate(qc_test = "grossrange", threshold = paste0(sensor_type, "_", threshold))
+      mutate(
+        sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type),
+        qc_test = "grossrange", threshold = paste0(sensor_type, "_", threshold),
+        threshold = str_replace(threshold, "aquameasure", "am")
+      )
   ) %>% bind_rows(
     threshold_tables$spike_table %>%
       pivot_longer(cols = c("spike_high", "spike_low"), names_to = "threshold") %>%
       mutate(qc_test = "spike")
-  )
+  ) %>%
+  mutate(sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type))
 
 
 #dat_qc <- qc_test_all(dat)
@@ -64,8 +71,8 @@ ui <- fluidPage(
 
       selectInput("qc_test", "QC Test", choices = qc_tests),
 
-      uiOutput("sensor_boxes"),
-      uiOutput("dynamic_ui")
+      uiOutput("sensor_ui"),
+      uiOutput("thresh_ui")
     ),
 
     # Show a plot of the generated distribution
@@ -80,7 +87,22 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-  output$sensor_boxes <- renderUI({
+  # reactive values
+  thresh <- reactive({
+    # filter for thresholds of interest
+    thresh <- thresh_all %>%
+      filter(qc_test == input$qc_test, variable == input$variable)
+
+    if (input$qc_test == "grossrange") {
+      thresh <- filter(thresh, sensor_type %in% input$sensors)
+    }
+
+    return(thresh)
+
+  })
+
+  # reactive user interface
+  output$sensor_ui <- renderUI({
 
     if (input$qc_test == "grossrange") {
       sensors <- dat %>%
@@ -97,24 +119,21 @@ server <- function(input, output) {
     }
   })
 
-  output$dynamic_ui <- renderUI({
+  output$thresh_ui <- renderUI({
 
-    # filter for thresholds of interest
-    thresh_display <- thresholds %>%
-      filter(qc_test == input$qc_test, variable == input$variable)
-
-    if (input$qc_test == "grossrange") {
-      thresh_display <- filter(thresh_display, sensor_type %in% input$sensors)
-    }
+    validate(
+      need(!(input$variable == "sensor_depth_measured_m" & input$qc_test == "climatology"),
+      "No thresholds available for selected Variable and QC Test")
+    )
 
     # make numeric inputs for thresholds of interest
     ui_elems <- list(NULL)
-    for (i in 1:nrow(thresh_display)) {
+    for (i in 1:nrow(thresh())) {
 
       ui_elems[[i]] <- numericInput(
-        inputId = thresh_display$threshold[i],
-        label = thresh_display$threshold[i],
-        value = thresh_display$value[i],
+        inputId = thresh()$threshold[i],
+        label = thresh()$threshold[i],
+        value = thresh()$value[i],
         width = '50%'
       )
     }
@@ -136,7 +155,7 @@ server <- function(input, output) {
       return(tagList(ui_grid))
     )
 
-    I# uncomment this if decide to not use the grid layout
+    # uncomment this if decide to not use the grid layout
     # return(tagList(ui_elems))
   })
 
